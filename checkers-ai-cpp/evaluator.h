9b70b0d8b6a366a46889588f983a01c16ac5dc4f
Isura Manchanayake <isura.14@cse.mrt.ac.lk>
2017-11-02 01:52:59 +0530

    Define common numerical values for result type

--- checkers-ai-cpp/evaluator.h
+++ checkers-ai-cpp/evaluator.h
@@ -3,9 +3,15 @@
 #include "board.h"
 
 namespace checkers_AI {
-    template<typename res_type>
+    template<typename T>
     class evaluator {
     public:
+        typedef T res_type;
+        static const res_type min_value;
+        static const res_type max_value;
+        static const res_type win_value;
+        static const res_type lost_value;
+        static const res_type draw_value;
         virtual const res_type evaluate(const board * board, const piece::color_type) = 0;
-    };
+    }; 
 }
--- checkers-ai-cpp/standard_evaluator.cpp
+++ checkers-ai-cpp/standard_evaluator.cpp
@@ -1,6 +1,14 @@
+#include <climits>
+
 #include "standard_evaluator.h"
 
 namespace checkers_AI {
+
+    const int standard_evaluator::min_value = INT_MIN;
+    const int standard_evaluator::max_value = INT_MAX;
+    const int standard_evaluator::win_value = 1000;
+    const int standard_evaluator::lost_value = -1000;
+    const int standard_evaluator::draw_value = 0;
 
     standard_evaluator::standard_evaluator() {
     }
