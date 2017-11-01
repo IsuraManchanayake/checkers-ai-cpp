e806af75760a338487ad0ac2c1351317b2d69d98
Isura Manchanayake <isura.14@cse.mrt.ac.lk>
2017-11-02 01:51:16 +0530

    Add some constants

--- checkers-ai-cpp/config.h
+++ checkers-ai-cpp/config.h
@@ -5,4 +5,7 @@ namespace checkers_AI {
     const int board_width = 10;
     const int board_height = 10;
     const int board_max_dim = board_width > board_height ? board_width : board_height;
+
+    // game parameters
+    const int max_non_captures = 50;
 }
--- checkers-ai-cpp/enum.h
+++ checkers-ai-cpp/enum.h
@@ -1,5 +1,11 @@
 #pragma once
 
 namespace checkers_AI {
-
+    enum class game_result {
+        N_DEFINED,
+        PLAY,
+        RED,
+        BLACK,
+        DRAW
+    };
 }
