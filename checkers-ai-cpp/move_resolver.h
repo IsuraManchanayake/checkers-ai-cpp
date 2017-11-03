d69d0d69b132a9d5d982387f96575dd386477913
Isura Manchanayake <isura.14@cse.mrt.ac.lk>
2017-11-04 01:00:24 +0530

    Fix logic errors and memory leaks in move_resolver

--- checkers-ai-cpp/move_resolver.h
+++ checkers-ai-cpp/move_resolver.h
@@ -1,42 +1,44 @@
 #pragma once
 
 #include <algorithm>
+#include <experimental\vector>
 
 #include "enum.h"
-#include "standard_evaluator.h"
+#include "default_evaluator.h"
 
 namespace checkers_AI {
 
-    template <typename eval_type = standard_evaluator>
+    template <typename eval_type = default_evaluator>
     class move_resolver
     {
         static_assert(std::is_base_of<evaluator<typename eval_type::res_type>, eval_type>::value,
             "eval_type should inherit from evaluator<eval_type::res_type>");
     public:
-        move_resolver();
         move_resolver(const int & depth);
+        move_resolver();
         ~move_resolver();
 
         int evaluated = 0;
 
         typedef typename eval_type::res_type res_type;
-        const move* resolve(board* board, move * last_opp_move, piece::color_type color);
+        move* resolve(board * board, move * last_opp_move, piece::color_type color);
     private:
         eval_type * _eval;
-        int _depth;
+        int _depth = 5;
         piece::color_type _color;
 
         const res_type _a_b_prune(board * board, res_type alpha, res_type beta, piece::color_type color, move * last_opp_move, int depth);
+        void _free_move_list_memory(std::vector<move*> moves);
     };
-
-    template <typename eval_type>
-    inline move_resolver<eval_type>::move_resolver() {
-        _eval = new eval_type();
-    }
 
     template<typename eval_type>
     move_resolver<eval_type>::move_resolver(const int & depth)
         : _depth(depth) {
+        _eval = new eval_type();
+    }
+
+    template <typename eval_type>
+    inline move_resolver<eval_type>::move_resolver() {
         _eval = new eval_type();
     }
 
@@ -46,13 +48,16 @@ namespace checkers_AI {
     }
 
     template<typename eval_type>
-    inline const move * move_resolver<eval_type>::resolve(board * board, move * last_opp_move, piece::color_type color) {
+    inline move * move_resolver<eval_type>::resolve(board * board, move * last_opp_move, piece::color_type color) {
         _color = color;
-        std::vector<move*> valid_moves_ = board->list_all_blance_pieces(last_opp_move);
+        std::vector<move*> valid_moves_ = board->list_all_moves(last_opp_move, color);
         if (std::any_of(valid_moves_.cbegin(), valid_moves_.cend(),
             [](move* move) { return move->move_kind == move::move_kind_type::CAPTURE; })) {
-            valid_moves_.erase(std::remove_if(valid_moves_.cbegin(), valid_moves_.cend(),
-                [](move* move) { return move->move_kind == move::move_kind_type::MOVE; }));
+            for (auto& move : valid_moves_) 
+                if (move->move_kind == move::move_kind_type::MOVE) 
+                    delete move;
+            valid_moves_.erase(std::remove_if(valid_moves_.begin(), valid_moves_.end(),
+                [](move* move) { return move->move_kind == move::move_kind_type::MOVE; }), valid_moves_.end());
         }
         sort(valid_moves_.begin(), valid_moves_.end(),
             [](move* move_1, move* move_2) { return move_1->capture_pieces.size() > move_2->capture_pieces.size(); });
@@ -65,9 +70,10 @@ namespace checkers_AI {
             res_type v_ = _a_b_prune(board, alpha, beta, !color, move, _depth - 1);
             board->reverse_move(move);
             if (v_ > alpha) {
-                alpha = v;
+                alpha = v_;
                 best_move_ = move;
             }
+            else delete move;
         }
         return best_move_;
     }
@@ -82,11 +88,11 @@ namespace checkers_AI {
         else if (gr_ == game_result::RED) {
             return color == piece::color_type::RED ? eval_type::win_value : eval_type::lost_value;
         }
-        if (d == 0) {
+        if (depth == 0) {
             evaluated++;
-            return _eval.evaluate(board, color);
+            return _eval->evaluate(board, color);
         }
-        std::vector<move*> valid_moves_ = board->list_all_moves(last_opp_move);
+        std::vector<move*> valid_moves_ = board->list_all_moves(last_opp_move, color);
         if (valid_moves_.size() == 0) {
             return eval_type::draw_value;
         }
@@ -105,9 +111,11 @@ namespace checkers_AI {
                     alpha = v_;
                 }
                 if (alpha >= beta) {
+                    _free_move_list_memory(valid_moves_);
                     return alpha;
                 }
             }
+            _free_move_list_memory(valid_moves_);
             return v_;
         }
         else {
@@ -125,10 +133,19 @@ namespace checkers_AI {
                     beta = v_;
                 }
                 if (alpha >= beta) {
+                    _free_move_list_memory(valid_moves_);
                     return beta;
                 }
             }
+            _free_move_list_memory(valid_moves_);
             return v_;
+        }
+    }
+
+    template<typename eval_type>
+    inline void move_resolver<eval_type>::_free_move_list_memory(std::vector<move*> moves) {
+        for (auto& move : moves) {
+            delete move;
         }
     }
 }
